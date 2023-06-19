fb578db3a7e1d3676343066a1024dfb5199a3725
Marnus PC <marnusvw7@gmail.com>
2023-06-19 13:04:39 +0200

    Displaying Critic Score

---
+++ src/Components/CriticScore.tsx
@@ -0,0 +1,18 @@
+import { Badge } from "@chakra-ui/react";
+import { color } from "framer-motion";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge fontSize="16px" paddingX={2} borderRadius="5px" colorScheme={color}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
