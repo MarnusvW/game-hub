4f4dc9df0e27396944201dfa76ceffe836e6cd67
Marnus PC <marnusvw7@gmail.com>
2023-07-12 11:31:48 +0200

    Add Github Repo Link to Project

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Link, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
@@ -12,6 +12,9 @@ const NavBar = ({ onSearch }: Props) => {
     <HStack padding="1rem">
       <Image src={logo} boxSize="60px"></Image>
       <SearchInput onSearch={onSearch}></SearchInput>
+      <Link target="_blank" href="https://github.com/MiniNInjaP/game-hub">
+        <Text fontSize={15}>Github Repository</Text>
+      </Link>
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
